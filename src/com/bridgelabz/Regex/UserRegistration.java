2bc3a6af696753beb8c9d715ea03b1819d60dbea
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-18 22:44:37 +0530

    Done

--- src/com/bridgelabz/Regex/UserRegistration.java
+++ src/com/bridgelabz/Regex/UserRegistration.java
@@ -4,21 +4,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
- * Check user last name with regex
+ * Check user mobile Format  with regex
  */
-/*E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl
-& co) and 2 optional (xyz & in) with
-precise @ and . positions*/
+
 public class UserRegistration {
-	
-	public boolean regexCheck(String validEmail) {
+
+	public boolean regexCheck(String mobileFormat) {
 
 		String regex = "^[0-9]{2}+[ ][0-9]{10}$";
 
 		// Create a Pattern object
 		Pattern r = Pattern.compile(regex);
 		// Now create a matcher object
-		Matcher m = r.matcher(validEmail);
+		Matcher m = r.matcher(mobileFormat);
 
 		if (m.matches()) {
 			return true;
@@ -28,12 +26,12 @@ public class UserRegistration {
 
 //Driver class.
 	public static void main(String[] args) {
-		UserRegistration checkEmail = new UserRegistration();
-		System.out.println(checkEmail.regexCheck("91 7474237474"));
-		System.out.println(checkEmail.regexCheck("91 337474237474"));
-		System.out.println(checkEmail.regexCheck("91237474237474"));
-		System.out.println(checkEmail.regexCheck("91 999990000"));
-		//System.out.println(checkEmail.regexCheck("abc-100@yahoo.3454com"));
+		UserRegistration mobileFormat = new UserRegistration();
+		System.out.println(mobileFormat.regexCheck("91 7474237474"));
+		System.out.println(mobileFormat.regexCheck("91 337474237474"));
+		System.out.println(mobileFormat.regexCheck("91237474237474"));
+		System.out.println(mobileFormat.regexCheck("91 999990000"));
+
 	}
 
 }
